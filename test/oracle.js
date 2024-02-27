86e6f1ac5f8f8dcc876578195884deb83edbb21b
SchneiderNejc <nejc.sch@gmail.com>
2024-02-27 23:09:47 +0100

    add TODO

---
+++ test/oracle.js
@@ -0,0 +1,6 @@
+// TODO
+/* Let's create a CallerContractInterface named callerContractInstance.
+Initialize callerContractInstance with the address of the caller contract, just like we did with myContractInstance above.
+Note that the address of the caller contract should come from the function parameters.
+Run the callerContractInstance.callback function, passing it _ethPrice and _id.
+    Lastly, emit the SetLatestEthPriceEvent.It takes two parameters: _ethPrice and _callerAddress. */
