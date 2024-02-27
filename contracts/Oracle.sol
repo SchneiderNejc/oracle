9774d5f449fdd081407cf7d368e113056d34cceb
SchneiderNejc <nejc.sch@gmail.com>
2024-02-27 23:08:14 +0100

    add events

--- contracts/Oracle.sol
+++ contracts/Oracle.sol
@@ -10,6 +10,11 @@ contract Oracle is Ownable {
     uint private ethPrice;
     address private oracleAddress;
     mapping(uint=>bool) myRequests;
+
+    event NewOracleAddress(address indexed oracleAddress);
+    event ReceivedNewRequestId(uint256 indexed id);
+    event PriceUpdated(uint ethPrice, uint indexed id);
+
     modifier onlyOracle() {
         require(msg.sender == address(iEthPriceOracle), "Not authorized.");
         _;
@@ -18,16 +23,19 @@ contract Oracle is Ownable {
     function setOracleInstanceAddress(address _oracleInstanceAddress) external onlyOwner {
         oracleAddress = _oracleInstanceAddress;
         iEthPriceOracle = IEthPriceOracle(oracleAddress);
+        emit NewOracleAddress(oracleAddress);
     }
 
     function updateEthPrice() external {
         uint id = iEthPriceOracle.getLatestEthPrice();
         myRequests[id] = true;
+        emit ReceivedNewRequestId(id);
     }
 
     function callback(uint _ethPrice, uint _id) public {
         require(myRequests[_id], "Request not in pending list.");
         ethPrice = _ethPrice;
         delete myRequests[_id];
+        emit PriceUpdated(_ethPrice, _id);
     }
 }
