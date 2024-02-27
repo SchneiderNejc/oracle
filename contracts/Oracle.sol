00933284d9f0b3d9d95236946ab45f60b61cfebb
SchneiderNejc <nejc.sch@gmail.com>
2024-02-27 23:07:53 +0100

    add functions and data structures

--- contracts/Oracle.sol
+++ contracts/Oracle.sol
@@ -5,8 +5,29 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "./IEthPriceOracle.sol";
 
 contract Oracle is Ownable {
+    IEthPriceOracle private iEthPriceOracle;
+    
+    uint private ethPrice;
     address private oracleAddress;
-    function setOracleInstanceAddress(address _oracleInstanceAddress) public {
+    mapping(uint=>bool) myRequests;
+    modifier onlyOracle() {
+        require(msg.sender == address(iEthPriceOracle), "Not authorized.");
+        _;
+    }
+
+    function setOracleInstanceAddress(address _oracleInstanceAddress) external onlyOwner {
         oracleAddress = _oracleInstanceAddress;
+        iEthPriceOracle = IEthPriceOracle(oracleAddress);
+    }
+
+    function updateEthPrice() external {
+        uint id = iEthPriceOracle.getLatestEthPrice();
+        myRequests[id] = true;
+    }
+
+    function callback(uint _ethPrice, uint _id) public {
+        require(myRequests[_id], "Request not in pending list.");
+        ethPrice = _ethPrice;
+        delete myRequests[_id];
     }
 }
