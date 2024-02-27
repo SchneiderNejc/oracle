270324530a23495d9eb4a92a29560d466bd1d646
SchneiderNejc <nejc.sch@gmail.com>
2024-02-27 23:05:51 +0100

    add Oracle.sol

--- contracts/Oracle.sol
+++ contracts/Oracle.sol
@@ -1,7 +1,10 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-contract Oracle {
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "./IEthPriceOracle.sol";
+
+contract Oracle is Ownable {
     address private oracleAddress;
     function setOracleInstanceAddress(address _oracleInstanceAddress) public {
         oracleAddress = _oracleInstanceAddress;
